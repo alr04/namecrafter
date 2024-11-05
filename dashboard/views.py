a005bc195150e75a483aebb02d0bacbaa5b3ded7
RTOUSchoolAccount <jbrown5@oakland.edu>
2024-11-05 00:38:12 -0500

    Commit!

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import User
 
 # This part of the code is where the text that is displayed on the dashboard is stored.
 @login_required
