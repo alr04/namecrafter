810245d643422268478245e55785735c83063dda
RTOUSchoolAccount <jbrown5@oakland.edu>
2024-11-04 19:08:43 -0500

    test

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+# This part of the code is where the text that is displayed on the dashboard is stored.
+def homepage(request):
+    return HttpResponse("Welcome to NameCrafter!")
 
-# Create your views here.
+def other(request):
+    return HttpResponse("Maybe there'll be another page here.")
