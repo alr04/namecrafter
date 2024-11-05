f27b81ec0fe24e333c6a76a62432c4e9c651f811
RTOUSchoolAccount <jbrown5@oakland.edu>
2024-11-04 19:21:32 -0500

    Test

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -22,5 +22,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage),
-    path('other/', views.other),
+    path('about/', views.about),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+
 # This part of the code is where the text that is displayed on the dashboard is stored.
 def homepage(request):
     return HttpResponse("Welcome to NameCrafter!")
 
-def other(request):
-    return HttpResponse("Maybe there'll be another page here.")
+
+def about(request):
+    return HttpResponse("About page.")
