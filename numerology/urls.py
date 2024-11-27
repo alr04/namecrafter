0a9bc1bd7abba4fe953be521670fdd88d5a93117
Drake Weller <88635442+DrakeJay@users.noreply.github.com>
2024-11-27 13:42:45 -0500

    typo fix

--- numerology/urls.py
+++ numerology/urls.py
@@ -3,6 +3,6 @@ from . import views
 from numerology import views
 
 urlpatterns = [
-    path('', views.name_input_view, name='name_input'),
+    path('', views.name_input, name='name_input'),
     path('result/', views.numerology_result, name='numerology_result'),
 ]
